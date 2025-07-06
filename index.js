beb9efa27ee5d331f06518b698ef84e868c21153
NikitaYatsko <archon04@mail.ru>
2025-07-06 13:59:22 +0300

    install jwt

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const app = express();
 
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+app.post('/auth/login', (req, res) => {
+    const token = jwt.sign({
+        email: req.body.email,
+        fullname: "Вася Вася"
+    }, "secret123")
+    res.json({
+        success: true,
+        token: token,
+    });
+    console.log(req.body);
+});
+
 
 app.listen(3000, (err) => {
     if (err) {
